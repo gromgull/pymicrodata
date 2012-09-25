a71f45119b6eec98c93fd73822a29871d8d91d34
Gunnar Aastrand Grimnes <gromgull@gmail.com>
2012-09-25 13:05:57 +0200

    added extra dependency for pyrdfa

--- setup.py
+++ setup.py
@@ -5,11 +5,17 @@ kwargs={}
 # is not installed.
 try:
     from setuptools import setup
-    kwargs["install_requires"]=['rdflib', 'html5lib']
+    kwargs["install_requires"]=["rdflib>3", "html5lib"]
     kwargs["entry_points"]={
-        'rdf.plugins.parser' : [ 'mdata = pyMicrodata.rdflib:MicrodataParser',
-                                 'microdata = pyMicrodata.rdflib:MicrodataParser'],
+        "rdf.plugins.parser" : [ "mdata = pyMicrodata.rdflib:MicrodataParser",
+                                 "microdata = pyMicrodata.rdflib:MicrodataParser"],
         }
+
+    # several details or simply a more complete serialization depends on pyRdfa
+    # Although the basic RDF conversion works without this, some serializations
+    # may not be available and the vocabulary expansion mechanism would not
+    # work either. 
+    kwargs["extras_requires"]={ "Rdfa" : ["pyRdfa>3"] }
 
 except:
     from distutils.core import setup
@@ -21,7 +27,7 @@ setup(name="pyMicrodata",
       author_email="ivan@w3.org",
 	  maintainer="Ivan Herman",
 	  maintainer_email="ivan@w3.org",
-      packages=['pyMicrodata'],
+      packages=["pyMicrodata"],
       **kwargs
       )
 
